d556063267fc9c779f07f700687bfd32bdca47a4
Nicolas Cappella <nicolascappellabb@gmail.com>
2022-06-14 00:51:12 -0300

    ,

--- src/main.ts
+++ src/main.ts
@@ -13,21 +13,11 @@ import { createProxyMiddleware } from 'http-proxy-middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // app.enableCors({
-  //   origin: ['https://erp-dolar-frontend.vercel.app/'],
-  //   credentials: true,
-  //   methods: [' GET', 'POST', 'PATCH', 'DELETE'],
-  // });
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
-    next();
-  });
 
   app.enableCors({
-    allowedHeaders: '*',
-    origin: '*',
+    origin: ['https://erp-dolar-frontend.vercel.app/'],
+    credentials: true,
+    methods: [' GET', 'POST', 'PATCH', 'DELETE'],
   });
   const configService = app.get(ConfigService);
   const mongoUsername = configService.get<string>('MONGO_USERNAME');
